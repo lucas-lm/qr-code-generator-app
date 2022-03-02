143bd544ed453f1fe9940e9251da1a9c90df03cc
Lucas <lucas-m@outlook.com>
2022-03-01 22:19:24 -0300

    add initial page structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { useState } from "react";
 import QRCode from 'qrcode.react';
 
 function App() {
+  const [value, setValue] = useState('https://lucas-lm.github.io');
+
   return (
     <div className="App">
-      <QRCode value="http://facebook.github.io/react/" />
+      <div className='page-content'>
+        <h1>QR Code Generator</h1>
+        <textarea value={value} onChange={e => setValue(e.target.value)}/>
+        <div className='output'>
+          <QRCode value={value} renderAs='svg' />
+        </div>
+        <button>Print</button>
+        <button>Download</button>
+      </div>
     </div>
   );
 }
